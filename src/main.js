dbaf01331bf1a2e1e056cd982426d0f16c290188
kaynekaynekayne <theygoon@gmail.com>
2023-09-15 15:28:23 +0900

    m

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
+import Vue,{createApp} from 'vue'
 import App from './App.vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
 //라우터 선언
 import router from './router';
-
+//vuex
+import store from '@/store/store.js';
+//bootstrap with vue
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
@@ -16,3 +17,9 @@ new Vue({
   render: h => h(App),
   router,
 }).$mount('#app')
+
+//vuex
+const app=createApp(App);
+app.use(router);
+app.use(store);
+window.app=app.mount();
